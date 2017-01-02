76492e0012f1c1871164df1f229dda7aba658c5d
Babatunde Adeyemi <barbietunnie@gmail.com>
2017-01-02 20:45:04 +0100

    Add Crypto-md5 and implement component

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,24 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import md5 from 'crypto-md5';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  email: any;
+  password: any;
+  profilePicture: any = "https://www.gravatar.com/avatar/";
 
   constructor(public navCtrl: NavController) {
     
+  }
+
+  emailChanged() {
+    this.profilePicture = "https://www.gravatar.com/avatar/" + 
+                            md5(this.email.toLowerCase(), 'hex');
   }
 
 }
